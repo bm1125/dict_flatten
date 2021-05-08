fe5109b40ab9e2e7dbf1431cba700a36555e2f3c
Yarden Gur <41701875+bm1125@users.noreply.github.com>
2021-05-08 10:13:03 +0300

    Add files via upload

---
+++ dict_flatten.py
@@ -0,0 +1,18 @@
+def flatten_dict(dict_obj):
+    temp_dict = {}
+    for key, value in dict_obj.items():
+        if isinstance(value, dict):
+            temp_dict.update(flatten_dict(create_new_dict(value, key)))
+        elif isinstance(value, list):
+            for i in range(len(value)):
+                temp_dict.update(flatten_dict(create_new_dict(value[i], key + "[{}]".format(i))))
+        else:
+            temp_dict[key] = value
+    return temp_dict
+
+def create_new_dict(dict_obj, prefix = None):
+    prefix = prefix or ""
+    temp_dict = {}
+    for key, value in dict_obj.items():
+        temp_dict[prefix + "." + key] = value
+    return temp_dict
